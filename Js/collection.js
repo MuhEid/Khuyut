dab4f86eaef5cd0e6647dfd2801175b94bed9f33
MuhEid96 <moh.eid.1996@gmail.com>
2021-09-16 21:58:14 +0200

    added new and better slider in collections page and enhanced the html and css code

--- Js/collection.js
+++ Js/collection.js
@@ -1,101 +1,36 @@
-const slideOneContainer = document.querySelector('.image-container');
-const imageContainer = document.querySelectorAll('.image-container img');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-let counter = 1;
-let size = imageContainer[0].clientWidth; 
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-slideOneContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-nextBtn.addEventListener('click',()=>{
-    if (counter >= imageContainer.length -1) return;
-    slideOneContainer.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    slideOneContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    console.log(counter);
-
-
-});
-
-prevBtn.addEventListener('click',()=>{
-    if (counter <= 0) return;
-    slideOneContainer.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    slideOneContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    console.log(counter);
-
-    
-
-});
-
-slideOneContainer.addEventListener('transitionend', ()=>{
-    if (imageContainer[counter].id === 'lastImage'){
-        slideOneContainer.style.transition = "none";
-        counter = imageContainer.length - 2;
-        slideOneContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-    }
-});
-
-slideOneContainer.addEventListener('transitionend', ()=>{
-    if (imageContainer[counter].id === 'firstImage'){
-        slideOneContainer.style.transition = "none";
-        counter = imageContainer.length - counter;
-        slideOneContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-    }
-});
-
-// Second collection slider
-
-const slideTwoContainer = document.querySelector('.image2-container');
-const imageTwoContainer = document.querySelectorAll('.image2-container img');
-
-const prevBtnSndSlider = document.querySelector('#prevBtnSndSlider');
-const nextBtnSndSlider = document.querySelector('#nextBtnSndSlider');
-
-let counterTwo = 1;
-size = imageTwoContainer[0].clientWidth; 
-
-slideTwoContainer.style.transform = 'translateX(' + (-size * counterTwo) + 'px)';
-
-nextBtnSndSlider.addEventListener('click',()=>{
-    if (counterTwo >= imageContainer.length -1) return;
-    slideTwoContainer.style.transition = "transform 0.4s ease-in-out";
-    counterTwo++;
-    slideTwoContainer.style.transform = 'translateX(' + (-size * counterTwo) + 'px)';
-    console.log(counterTwo);
-
-
-});
-
-prevBtnSndSlider.addEventListener('click',()=>{
-    if (counterTwo <= 0) return;
-    slideTwoContainer.style.transition = "transform 0.4s ease-in-out";
-    counterTwo--;
-    slideTwoContainer.style.transform = 'translateX(' + (-size * counterTwo) + 'px)';
-    console.log(counterTwo);
-
-    
-
-});
-
-slideTwoContainer.addEventListener('transitionend', ()=>{
-    if (imageContainer[counterTwo].id === 'lastImageSndSlider'){
-        slideTwoContainer.style.transition = "none";
-        counterTwo = imageTwoContainer.length - 2;
-        slideTwoContainer.style.transform = 'translateX(' + (-size * counterTwo) + 'px)';
-
-    }
-});
-
-slideTwoContainer.addEventListener('transitionend', ()=>{
-    if (imageTwoContainer[counterTwo].id === 'firstImageSndSlider'){
-        slideTwoContainer.style.transition = "none";
-        counterTwo = imageTwoContainer.length - counterTwo;
-        slideTwoContainer.style.transform = 'translateX(' + (-size * counterTwo) + 'px)';
-
-    }
-});
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("image-container");
+  let dots = document.getElementsByClassName("blogSlider__dots--dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  const div = document.createElement("div");
+  div.className = "blog-slider__numbertext";
+  div.innerHTML = `${slideIndex} / 3`;
+  document
+    .getElementsByClassName("image-container")
+    [slideIndex - 1].appendChild(div);
+}
